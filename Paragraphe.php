bdb47b9a5d96705462960d860d9fdaedbd1a9c76
XwolfY <Gautier.plancq@gmail.com>
2016-12-15 22:56:38 +0100

    Quelques ajouts
    Ajouts en rapport aux chapitres Openclassroom.

--- Paragraphe.php
+++ Paragraphe.php
@@ -1,3 +1,42 @@
-		<p>
-            <?php echo "Je suis du PHP." ?>
-        </p>
+<!DOCTYPE html>
+
+<html>
+<h1>Votre pays !</h1>
+<p>
+    <?php 
+    
+    if ($_POST['Country'] != 'Autres')  
+        {
+            echo 'Vous habitez en ' . $_POST['Country'].'. ';
+        }
+        
+    if ($_POST['Country']=='France')
+        {
+            echo 'La France est un merveilleux pays.';
+        }
+    
+            else 
+        {
+            echo '<br/>Je pense que vous devriez partir en France';
+        }
+        
+    
+    ?>
+</p>
+    <?php 
+    if (isset($_POST['Pseudo']) and $_POST['Pseudo'] != '')
+    {
+        
+    echo 'Bonjour ' . strip_tags($_POST['Pseudo']);
+    
+    }
+    else
+    {
+        echo 'Qui es-tu ?';
+    }
+
+    echo '<br/> Votre mot de passe: ' . htmlspecialchars($_POST['Password']);
+    
+    ?>
+    
+   </html>
--- SandBox.php
+++ SandBox.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Notre première instruction : echo</title>
+        <title>Ma SandBox</title>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="SandBoxC.css">
+        <script>
+            function op()
+            {
+             var fe='http://www.aliz-air.fr';
+             window.open(fe);
+                
+            }
+            //op()
+        </script>
     </head>
     <body>
-        <h2 id="test">Affichage de texte avec PHP</h2>
-        <?php include("Paragraphe.php") ?>
+        <!--<h2 id="test">Affichage de texte avec PHP</h2>
+         <?//php include("Paragraphe.php") ?>
+
         Moi je suis du simple HTML mais je suis quand même là hein !
         
-        <?php
+        <p id="php">
+            <?/* php
     function volumeCube($cote)
 {   
  return $cote*$cote*$cote;   
@@ -22,12 +33,51 @@
     $array[1]="pommes";
     $array[]="pêches";
     $array[]="poires";
-    echo "Tu aimes les $array[1] et aussi les $array[2] et $array[3]";
+    echo "<br />Tu aimes les $array[1] et aussi les $array[2] et $array[3]";
     $volume=volumeCube(2);
     echo ". Le volume est de $volume cm3";
         
     $Gautier= array ("Prenom"=>"Gautier","Nom"=>"Plancq","Age"=>"17");
-    echo(". Tu t n'appelles ".$Gautier["Prenom"]." ".$Gautier["Nom"]." , ". $Gautier["Age"]." ans.");
-        ?>
+    echo(". Tu t n'appelles ".$Gautier["Prenom"]." ".$Gautier["Nom"]." , ". $Gautier["Age"]." ans. <br /><br/>");
+        
+        if (array_key_exists('Prenom',$Gautier))
+        {
+         echo 'Cela existe';   
+        }
+            */?></p> -->
+        
+        
+        <h2>Où habitez-vous?</h2><br />
+        <a id="link" href="Paragraphe.php?pays=France">France</a>
+        <a id="link" href="Paragraphe.php?pays=Chine">Chine</a>
+        <a id="link" href="Paragraphe.php?pays=Suède">Suède</a>
+        <br/> <br/>
+        
+        <fieldset>
+            <legend>Connexion</legend>
+            
+            <form method="post" action="Paragraphe.php">
+                <fieldset>
+                    <legend>Identifiants</legend>
+            <label for='Pseudo'>Votre pseudo</label> : <input type="text" name="Pseudo" placeholder="Pseudo" id="Pseudo" maxLength='20' autofocus/><br/><br/>
+            <Label for='Password'>Mot de passe</Label> : <input type="password" name="Password" id='Password' placeholder='Password' required/>
+                </fieldset>
+                <fieldset>
+                    <legend>Pays</legend>
+                    <input type="radio" name="Country" value="France" id="France"/><label for="France">France</label>
+                    <input type="radio" name="Country" value="Suède" id="Suède"/><label for="Suède">Suède</label>
+                    <input type="radio" name="Country" value="Chine" id="Chine"/><label for="Chine">Chine</label>
+                    <input type="radio" name="Country" value="Autres" id="Autres" checked="checked"/><label for="Autres">Autres</label>
+                </fieldset><br/>
+                <input type="submit" value="Envoyer"/>
+        </form>
+        </fieldset>
+        <br/> 
+        
+        
+    <pre>Cette balise est plutôt cool.
+    Je ne la connaissais pas avant mais maintenant je la trouve géniale !</pre>
+        
+        
     </body>
 </html>
